f13ef6966970aeb525c5b16c8aa1414ca5d61b9c
Jorge I. Meza <jimezam@gmail.com>
2025-04-21 18:39:57 -0500

    Improve assistant with temperature control from content_type

--- src/content_generation_assistant/assistant.py
+++ src/content_generation_assistant/assistant.py
@@ -5,18 +5,16 @@ class Assistant:
 
     @classmethod
     def generate(cls, provider: Provider, content_type: ContentType, prompt: str) -> str:
-        # SET TEMPERATURE
 
-        print(provider.temperature)
-        provider.temperature = content_type.get_preferred_temperature()
-        print(provider.temperature)
+        try:
+            provider.temperature = content_type.get_preferred_temperature()
+        except ValueError as e:
+            raise ValueError(f"Invalid temperature value: {e} for {provider.get_provider_name()} range is ({provider.get_min_temperature()}, {provider.get_max_temperature()})")
 
         return provider.query(content_type.get_context(), prompt)
 
 
-# ia.provider
-# type de contenido
-# - carta de presentaci'on
+
 # - c'odigo fuente
 # - post para facebook
 # - guion para tiktok
